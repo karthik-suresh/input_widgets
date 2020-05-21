6e078cd13e4153b87298442190d5dcbb2a6dde74
Karthik Hanasoge Suresh <kali.karthik@gmail.com>
2020-05-21 11:09:14 +0200

    Added simple dialog box widget
    * A dialog widget is added with a simple box

---
+++ lib/ui/common/widgets/dialogs/dialog.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+const String _title = 'Flutter Code Sample';
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: _title, home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.help),
+            onPressed: () async {
+              _showMyDialog(context);
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: FlatButton(
+            color: Colors.yellow,
+            child: Text('Here'),
+            onPressed: () async {
+              _showMyDialog(context);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showMyDialog(context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
